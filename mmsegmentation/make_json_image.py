78a5dab145c7e5557bf193b773f08c91f3802e95
Park Junghyun <abc1245@naver.com>
2022-05-02 05:03:07 +0000

    [Fix] test폴더에 이미지가 순서대로 들어가게 수정

--- mmsegmentation/make_json_image.py
+++ mmsegmentation/make_json_image.py
@@ -2,38 +2,38 @@ import os
 import json
 import shutil
 
-'''
-Set json_dir
-'''
+
 DATAROOT = "./"
 TRAINJSON = os.path.join(DATAROOT,"./train_all.json")  # 모든 데이터 들어있는 json 파일
 TESTJSON = os.path.join(DATAROOT,"./test.json")        # test 데이터 들어있는 json 파일
-'''
-Redistribution image by train/valid/test 
-rename img file name by img_id 
-'''
-def _rename_images(json_dir, image_dir):
+
+
+def _rename_images(json_dir, image_dir, id=False):
 	with open(json_dir, "r", encoding="utf8") as outfile:
 		json_data = json.load(outfile)
 	image_datas = json_data["images"]
 
-	for image_data in image_datas:
-		shutil.copyfile(os.path.join(image_data['file_name']), os.path.join(image_dir,f"{image_data['file_name'].split('.')[0].replace('/','')}.jpg"))
+	if id:
+		for image_data in image_datas:
+			shutil.copyfile(os.path.join(image_data['file_name']), os.path.join(image_dir,f"{image_data['id']:04}_{image_data['file_name'].split('.')[0].replace('/','')}.jpg"))
+	else:
+		for image_data in image_datas:
+			shutil.copyfile(os.path.join(image_data['file_name']), os.path.join(image_dir,f"{image_data['file_name'].split('.')[0].replace('/','')}.jpg"))
 
-'''
-Wrap func
-'''
-def make(json,path):
+
+def make(json, path, id=False):
 	imagePath = '../mmseg/'+path
 	os.makedirs(imagePath, exist_ok=True)
-	_rename_images(json,imagePath)
+	_rename_images(json, imagePath, id)
 
 
-'''
-Main
-'''
 def __main__():
-	make(TRAINJSON, 'images')
+	make(TRAINJSON, 'images', id=False)
+	make(TESTJSON, 'test', id=True)
+
+
+if __name__=='__main__':
+	__main__()JSON, 'images')
 	make(TESTJSON, 'test')
 
 
